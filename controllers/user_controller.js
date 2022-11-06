11e3970d99d77a7201480c9a722ea210ae9264e4
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2022-11-06 21:19:47 +0500

    Get Record

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -18,6 +18,22 @@ try {
 
 }
 
+// getAll student 
+const getalluser=async(req,res)=>{
+    // res.send("Hello My First API");
+    
+    try {
+       // const addstd = new student_model(req.body)
+        // console.log(addstd);
+        let getdata = await user_modules.find({});
+        res.status(201).send(getdata) //koi bhi data get krne k liye 
+            //status uska 201 hona chahye
+    } catch (e) {
+        console.log(e)
+        res.status(400).send(e)
+    }
+    
+    
+    }
 
-
-module.exports={demoapi}
+module.exports={demoapi,getalluser}
--- router/user_routes.js
+++ router/user_routes.js
@@ -1,10 +1,10 @@
-const{demoapi}=require("../controllers/user_controller"); // Here line controller call
+const{demoapi,getalluser}=require("../controllers/user_controller"); // Here line controller call
 const express=require ("express");
 const user_routes=express.Router()
 // Path of First API
 
 user_routes.post("/api/testingapi",demoapi)
-//user_routes.get("/api/getalluser",getalluser)
+user_routes.get("/api/getalluser",getalluser)
 
 
 
